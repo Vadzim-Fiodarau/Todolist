8837c022f30f0ac1a2725d920c9c40554ba2b4fc
Vadzim-Fiodarau <fioadarau.v@gmail.com>
2021-08-18 23:01:15 +0300

    Updates

---
+++ precache-manifest.e3c88b99e4576fa56fa7b39005a82879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea96662fc8c8823077c8e884a8ce0dd",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "e545a3728624e7ec919f",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "8dd4d1d576484c1409ce",
+    "url": "/Todolist/static/js/2.96e72576.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/Todolist/static/js/2.96e72576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e545a3728624e7ec919f",
+    "url": "/Todolist/static/js/main.23cb163e.chunk.js"
+  },
+  {
+    "revision": "23a8ead80f8150e06955",
+    "url": "/Todolist/static/js/runtime-main.ba498293.js"
+  }
+]);
